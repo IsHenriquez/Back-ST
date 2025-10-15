18443dd55a27059d7f8d34e4fa0151cb25e1edd4
IsHenriquez <is.henriquez@duocuc.cl>
2025-10-15 03:50:04 -0300

    fix para CORS

--- app/main.py
+++ app/main.py
@@ -20,17 +20,9 @@ app.include_router(vehicle.router)
 app.include_router(auth.router)
 
 
-ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://localhost:5173",
-    "http://127.0.0.1:5173",
-    "capacitor://localhost",
-    "ionic://localhost",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS, 
+    allow_origins=["*"], 
     allow_credentials=True,        
     allow_methods=["*"],
     allow_headers=["*"],           

e30802d0e0d6156ab646cc389602609981368466
IsHenriquez <95454595+IsHenriquez@users.noreply.github.com>
2025-11-18 19:08:54 -0300

    Remove timestamp columns from Position model
    Removed created_at and updated_at columns from Position model.

--- app/models/position.py
+++ app/models/position.py
@@ -10,5 +10,4 @@ class Position(Base):
     address = Column(String(255), nullable=True)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+

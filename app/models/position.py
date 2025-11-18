cfccb09d25cd5997060680e06424bdf630236d30
IsHenriquez <95454595+IsHenriquez@users.noreply.github.com>
2025-11-18 19:14:06 -0300

    Add timestamps for created_at and updated_at fields

--- app/models/position.py
+++ app/models/position.py
@@ -10,4 +10,6 @@ class Position(Base):
     address = Column(String(255), nullable=True)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
